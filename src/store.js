b486ccb54b2eef86a86a342110f2bb659c464e0b
Chanaka Sandaruwan <chanakasan@gmail.com>
2017-01-27 10:24:37 +0530

    add store.js

---
+++ src/store.js
@@ -0,0 +1,23 @@
+export const loadState = () => {
+  try {
+    const serializedState = window.localStorage.getItem('app_state')
+
+    if (serializedState === null) {
+      return undefined
+    } else {
+      return JSON.parse(serializedState)
+    }
+  } catch (error) {
+    console.log("[DEBUG] error =", error)
+    return undefined
+  }
+}
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state)
+    window.localStorage.setItem('app_state', serializedState)
+  } catch (error) {
+    console.log("[DEBUG] error =", error)
+  }
+}
